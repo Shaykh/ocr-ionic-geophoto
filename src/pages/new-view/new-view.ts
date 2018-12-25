ca0d9098138306d63ac6775baebba30243cf6e4a
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2018-12-25 13:55:01 +0000

    Ajout de fonctionnalités de new-view

--- src/pages/new-view/new-view.ts
+++ src/pages/new-view/new-view.ts
@@ -1,25 +1,33 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-/**
- * Generated class for the NewViewPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-new-view',
   templateUrl: 'new-view.html',
 })
-export class NewViewPage {
+export class NewViewPage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  natureViewForm: FormGroup;
+  latitude: number;
+  longitude: number;
+  imageUrl: string;
+
+  constructor(public formBuilder: FormBuilder) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad NewViewPage');
+  ngOnInit() {
+    this.initForm();
+  }
+  
+  initForm() {
+    this.natureViewForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      date: [new Date().toISOString(), Validators.required],
+      description: ['']
+    });
   }
 
+  onSubmitForm() {
+    
+  }
 }
