08669748185bbee6b070f96499a533e9e0493636
Diaby Cheick Mahomed Lamine <diabycheick@live.fr>
2018-12-25 13:27:17 +0000

    Ajout des fonctions de la homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Subscription } from 'rxjs/Subscription';
+import { NatureViewService } from '../../services/natureView.service';
+import { NatureView } from '../../models/NatureView.model';
+import { NewViewPage } from '../new-view/new-view';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
 
-  constructor(public navCtrl: NavController) {
+  natureViewList: NatureView[];
+  natureViewSubscription: Subscription;
+  newViewPage: NewViewPage;
+
+  constructor(public navCtrl: NavController,
+    private natureViewService: NatureViewService) {
+  }
+
+  ngOnInit() {
+    this.natureViewSubscription = this.natureViewService.natureViewList$.subscribe(
+      (natureViews: NatureView[]) => {
+        this.natureViewList = natureViews;
+      }
+    );
+    this.natureViewService.emitList();
+  }
+
+  onLoadNatureView(view: NatureView) {
 
   }
 
+  ngOnDestroy() {
+    this.natureViewSubscription.unsubscribe();
+  }
 }
